c839dfd428472b31e8c98509d1c41988001c0ed8
pjetras <90355400+pjetras@users.noreply.github.com>
2022-04-18 15:57:30 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+from keep_alive import keep_alive
+from music_cog import music_cog
+
+
+TOKEN = 'Your bot token here'
+
+client = commands.Bot(command_prefix = 'Your bot prefix here')
+client.remove_command('help')
+client.add_cog(music_cog(client))
+
+@client.event
+async def on_ready():
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='Your bot status'))
+
+@client.event
+async def on_connect():
+    print("Bot is on!")
+
+keep_alive()
+client.run(TOKEN)
