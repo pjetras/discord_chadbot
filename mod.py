d4706fd6fe3e42db177fd31c14addfc4d6a2699e
pjetras <90355400+pjetras@users.noreply.github.com>
2022-04-21 21:12:06 +0200

    Update mod.py

--- mod.py
+++ mod.py
@@ -8,32 +8,30 @@ class Mod(commands.Cog):
     @commands.Command
     async def kick(self, ctx, member: discord.Member, *, reason=None):
         if ctx.message.author.guild_permissions.administrator:
-            embed = discord.Embed(description = ":airplane: **Użytkownik ** "+f"**{member.mention}**"+" **został wyrzucony z serwera** ", colour=0x62008f, timestamp=datetime.utcnow())
+            embed = discord.Embed(description = ":airplane: **User ** "+f"**{member.mention}**"+" **got kicked from this server** ", colour=0x62008f, timestamp=datetime.utcnow())
             embed.set_author(icon_url = member.avatar_url, name = member.name+"#"+member.discriminator)
             embed.add_field(name = 'Powód wyrzucenia: ', value = f"{reason}", inline = False)
-            embed.set_footer(icon_url = ctx.author.avatar_url, text = f'Wykonane przez {ctx.author.name}')
+            embed.set_footer(icon_url = ctx.author.avatar_url, text = f'Requested by {ctx.author.name}')
             await member.kick(reason=reason)
             await ctx.send(embed=embed)
         else:
-            embed = discord.Embed(description = ":no_entry_sign: **Nie masz wystarczających uprawnień, aby użyć tej komendy ** ", colour=0x62008f, timestamp=datetime.utcnow())
+            embed = discord.Embed(description = ":no_entry_sign: **You dont have permissions to execute this command ** ", colour=0x62008f, timestamp=datetime.utcnow())
             embed.set_author(icon_url = member.avatar_url, name = member.name+"#"+member.discriminator)
-            embed.set_footer(icon_url = ctx.author.avatar_url, text = f'Wykonane przez {ctx.author.name}')
-            embed.set_thumbnail(url = 'https://cdn.discordapp.com/attachments/907356437808808027/966773714677022780/769591370884055051.png')
+            embed.set_footer(icon_url = ctx.author.avatar_url, text = f'Requested by {ctx.author.name}')
             await ctx.send(embed=embed)
             return
     @commands.Command
     async def ban(self, ctx, member: discord.Member, *, reason=None):
         if ctx.message.author.guild_permissions.administrator:
-            embed = discord.Embed(description = ":airplane: **Użytkownik ** "+f"**{member.mention}**"+" **został zbanowany na tym serwerze** ", colour=0x62008f, timestamp=datetime.utcnow())
+            embed = discord.Embed(description = ":airplane: **User ** "+f"**{member.mention}**"+" **got banned from this server** ", colour=0x62008f, timestamp=datetime.utcnow())
             embed.set_author(icon_url = member.avatar_url, name = member.name+"#"+member.discriminator)
             embed.add_field(name = 'Powód bana: ', value = f"{reason}", inline = False)
-            embed.set_footer(icon_url = ctx.author.avatar_url, text = f'Wykonane przez {ctx.author.name}')
+            embed.set_footer(icon_url = ctx.author.avatar_url, text = f'Requested by {ctx.author.name}')
             await member.ban(reason=reason)
             await ctx.send(embed=embed)
         else:
-            embed = discord.Embed(description = ":no_entry_sign: **Nie masz wystarczających uprawnień, aby użyć tej komendy ** ", colour=0x62008f, timestamp=datetime.utcnow())
+            embed = discord.Embed(description = ":no_entry_sign: **You dont have permissions to execute this command ** ", colour=0x62008f, timestamp=datetime.utcnow())
             embed.set_author(icon_url = member.avatar_url, name = member.name+"#"+member.discriminator)
-            embed.set_footer(icon_url = ctx.author.avatar_url, text = f'Wykonane przez {ctx.author.name}')
-            embed.set_thumbnail(url = 'https://cdn.discordapp.com/attachments/907356437808808027/966773714677022780/769591370884055051.png')
+            embed.set_footer(icon_url = ctx.author.avatar_url, text = f'Requested by {ctx.author.name}')
             await ctx.send(embed=embed)
             return
