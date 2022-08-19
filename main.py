1444e939309c5d9415eb5f0601c22a4a231e50de
pjetras <90355400+pjetras@users.noreply.github.com>
2022-08-19 14:12:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
-import discord
-from discord.ext import commands
+import nextcord
+from nextcord.ext import commands
+import asyncio
 from keep_alive import keep_alive
 from music_cog import music_cog
 from mod import Mod
+from imgsrch import Img
 
 
 TOKEN = 'Your bot token here'
-
+# Importing commands from other files
 client = commands.Bot(command_prefix = 'Your bot prefix here')
 client.remove_command('help')
 client.add_cog(music_cog(client))
 client.add_cog(Mod(client))
-
+client.add_cog(Img(client))
+# Custom status
 @client.event
 async def on_ready():
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='Your bot status'))
+    statuses = ['You | -help', f'{len(client.guilds)} servers | -help']
+
+    while not client.is_closed():
+        status = random.choice(statuses)
+
+        await client.change_presence(activity=nextcord.Activity(type=nextcord.ActivityType.watching, name = status))
+
+        await asyncio.sleep(10)
+    client.loop.create_task(on_ready())
+    await client.change_presence(activity=nextcord.Activity(type=nextcord.ActivityType.watching, name="Pornhub VR"))
+    await client.change_presence(activity=nextcord.Activity(type=nextcord.ActivityType.watching, name =f'{len(client.guilds)} servers'))
 
 @client.event
 async def on_connect():
