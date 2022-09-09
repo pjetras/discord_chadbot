cb26d0d680a0bfa4682b8a5c03a27b1144e16aca
pjetras <90355400+pjetras@users.noreply.github.com>
2022-09-09 22:00:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from music_cog import music_cog
 from mod import Mod
 from utillity import Img, Avatar, Userinfo, Serverinfo, Roles
 from help import Help
+from automod import Automod
 
 
 TOKEN = 'Your bot token here'
@@ -20,6 +21,7 @@ client.add_cog(Avatar(client))
 client.add_cog(Userinfo(client))
 client.add_cog(Serverinfo(client))
 client.add_cog(Roles(client))
+client.add_cog(Automod(client))
 # Custom status
 @client.event
 async def on_ready():
