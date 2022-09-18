f366a04cfc1b17e70cf147befde462c950cd2b4f
pjetras <90355400+pjetras@users.noreply.github.com>
2022-09-18 12:18:30 +0200

    Update logs.py

--- logs.py
+++ logs.py
@@ -50,18 +50,21 @@ class Logs(commands.Cog):
             await ctx.response.send_message("You are not allowed to use this command.", ephemeral=True)
     @slash_command(name = 'logchannel', description = 'select a channel to send log messages')
     async def setlog(self, ctx:Interaction, channel:GuildChannel = SlashOption(channel_types=[ChannelType.text], name = 'channel', description = 'Please mention a channel')):
-        db = sqlite3.connect('database.db')
-        cursor = db.cursor()
-        cursor.execute('SELECT guild_id FROM logs WHERE guild_id = ?', (ctx.user.guild.id,))
-        result = cursor.fetchone()
-        if result:
-            cursor.execute('UPDATE logs SET channel_id = ? WHERE guild_id = ?', (channel.id, ctx.user.guild.id))
+        if ctx.user.guild_permissions.administrator:
+            db = sqlite3.connect('database.db')
+            cursor = db.cursor()
+            cursor.execute('SELECT guild_id FROM logs WHERE guild_id = ?', (ctx.user.guild.id,))
+            result = cursor.fetchone()
+            if result:
+                cursor.execute('UPDATE logs SET channel_id = ? WHERE guild_id = ?', (channel.id, ctx.user.guild.id))
+            else:
+                cursor.execute('INSERT INTO logs(guild_id, channel_id) VALUES(?, ?)', (ctx.user.guild.id, channel.id))
+            await ctx.send(f'Log channel was set on {channel.mention}')
+            db.commit()
+            cursor.close()
+            db.close()
         else:
-            cursor.execute('INSERT INTO logs(guild_id, channel_id) VALUES(?, ?)', (ctx.user.guild.id, channel.id))
-        await ctx.send(f'Log channel was set on {channel.mention}')
-        db.commit()
-        cursor.close()
-        db.close()
+            await ctx.response.send_message("You are not allowed to use this command.", ephemeral=True)
     @commands.Cog.listener()
     async def on_message_delete(self, message):
         db = sqlite3.connect('database.db')
@@ -80,12 +83,13 @@ class Logs(commands.Cog):
             cursor.execute("SELECT channel_id FROM logs WHERE guild_id = ?", (message.guild.id,))
             result = cursor.fetchone()
             if result:
-                log_channel = self.bot.get_channel(result[0])
-                embed = nextcord.Embed(description = ":wastebasket: **Message sent by** "+f"**{message.author.mention}**"+" **was deleted on channel ** "+f"**{message.channel.mention}**",colour=0xed0000, timestamp=datetime.utcnow())
-                embed.set_author(icon_url = message.author.avatar.url, name = message.author.name+"#"+message.author.discriminator)
-                embed.add_field(name = 'Message deleted', value = f"```{message.content}```", inline = False)
-                embed.set_footer(icon_url = self.bot.user.avatar, text = 'ChadBot')
-                await log_channel.send(embed=embed)
+                if message.author.bot == False:
+                    log_channel = self.bot.get_channel(result[0])
+                    embed = nextcord.Embed(description = ":wastebasket: **Message sent by** "+f"**{message.author.mention}**"+" **was deleted on channel ** "+f"**{message.channel.mention}**",colour=0xed0000, timestamp=datetime.utcnow())
+                    embed.set_author(icon_url = message.author.avatar.url, name = message.author.name+"#"+message.author.discriminator)
+                    embed.add_field(name = 'Message deleted', value = f"```{message.content}```", inline = False)
+                    embed.set_footer(icon_url = self.bot.user.avatar, text = 'ChadBot')
+                    await log_channel.send(embed=embed)
             else:
                 pass
     @commands.Cog.listener()
@@ -106,13 +110,14 @@ class Logs(commands.Cog):
             cursor.execute("SELECT channel_id FROM logs WHERE guild_id = ?", (before.guild.id,))
             result = cursor.fetchone()
             if result:
-                log_channel = self.bot.get_channel(result[0])
-                embed = nextcord.Embed(description = ":pencil2: **Message sent by** "+f"**{before.author.mention}**"+" **was edited on channel ** "+f"**{before.channel.mention}**", colour=0xed0000, timestamp=datetime.utcnow())
-                embed.set_author(icon_url = after.author.avatar.url, name = after.author.name+"#"+after.author.discriminator)
-                embed.add_field(name = 'Message before', value = f"```{before.content}```", inline = False)
-                embed.add_field(name = 'Message after', value = f"```{after.content}```", inline = False)
-                embed.set_footer(icon_url = self.bot.user.avatar, text = 'ChadBot')
-                await log_channel.send(embed=embed)
+                if after.author.bot == False:
+                    log_channel = self.bot.get_channel(result[0])
+                    embed = nextcord.Embed(description = ":pencil2: **Message sent by** "+f"**{before.author.mention}**"+" **was edited on channel ** "+f"**{before.channel.mention}**", colour=0xed0000, timestamp=datetime.utcnow())
+                    embed.set_author(icon_url = after.author.avatar.url, name = after.author.name+"#"+after.author.discriminator)
+                    embed.add_field(name = 'Message before', value = f"```{before.content}```", inline = False)
+                    embed.add_field(name = 'Message after', value = f"```{after.content}```", inline = False)
+                    embed.set_footer(icon_url = self.bot.user.avatar, text = 'ChadBot')
+                    await log_channel.send(embed=embed)
             else:
                 pass
     @commands.Cog.listener()
